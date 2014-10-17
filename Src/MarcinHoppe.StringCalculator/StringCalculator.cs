a6e0e27645790fb9fa320068968dbf3de80ef71b
Marcin Hoppe <Marcin.Hoppe@powel.com>
2014-10-17 11:33:02 +0200

    Add files for code and tests

---
+++ Src/MarcinHoppe.StringCalculator/StringCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarcinHoppe.StringCalculator
+{
+    public static class StringCalculator
+    {
+        public static int Add(string numbers)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Test/MarcinHoppe.StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xunit;
+
+namespace MarcinHoppe.StringCalculator.Tests
+{
+    public class StringCalculatorTests
+    {
+        [Fact]
+        public void Empty()
+        {
+            Assert.Equal(0, StringCalculator.Add(""));
+        }
+    }
+}
